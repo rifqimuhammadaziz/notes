29f6e9f5cd8b44717c8d3960b460b73f171124ee
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-06-01 09:55:19 +0700

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Rifqi Muhammad Aziz on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

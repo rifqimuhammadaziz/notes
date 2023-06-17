00535ea414fa3373ce26b5d95c432571545e1bf6
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-06-17 19:43:02 +0700

    create credits view

--- Notes Watch App/View/DetailView.swift
+++ Notes Watch App/View/DetailView.swift
@@ -14,21 +14,14 @@ struct DetailView: View {
     let count: Int
     let index: Int
     
+    @State private var isCreditsPresented: Bool = false
+    
     // MARK: - BODY
     
     var body: some View {
         VStack(alignment: .center, spacing: 3) {
             // HEADER
-            HStack {
-                Capsule()
-                    .frame(height: 1)
-                
-                Image(systemName: "note.text")
-                
-                Capsule()
-                    .frame(height: 1)
-            } //: HSTACK
-            .foregroundColor(.accentColor)
+            HeaderView(title: "")
             
             // CONTENT
             Spacer()
@@ -53,6 +46,12 @@ struct DetailView: View {
                 
                 Image(systemName: "info.circle")
                     .imageScale(.large)
+                    .onTapGesture {
+                        isCreditsPresented.toggle()
+                    }
+                    .sheet(isPresented: $isCreditsPresented, content: {
+                        CreditsView()
+                    })
             } //: HSTACK
             .foregroundColor(.secondary)
         } //: VSTACK
